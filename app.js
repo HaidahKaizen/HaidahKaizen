d579e4888c2ec2ed607cf7be5aec5695abf5aff1
HaidahKaizen <henshinkaizen.artistic@gmail.com>
2025-06-27 03:22:18 +0800

    First Commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xffeedd, 0x444444, 0.3);
+        const ambient = new THREE.HemisphereLight(0xff8c42, 0x330000, 0.6);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
