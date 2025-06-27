eaaa2a557944e7c6c80e0eaa24ffa69e1d4682ad
HaidahKaizen <henshinkaizen.artistic@gmail.com>
2025-06-27 18:30:46 +0800

    First Commit

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ class App{
         this.backgroundSound = new THREE.Audio(this.listener);
 
         audioLoader.load('./assets/audio/College ambience.mp3', (buffer) => {
+            console.log('Audio loaded');
             this.backgroundSound.setBuffer(buffer);
             this.backgroundSound.setLoop(true);
-            this.backgroundSound.setVolume(0.5);
+            this.backgroundSound.setVolume(0.5+);
             this.backgroundSound.play();
         });
         
