695629e74f8a2e58bcb5c55684a0b0a0340d64f0
HaidahKaizen <henshinkaizen.artistic@gmail.com>
2025-06-27 20:34:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ class App{
 		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 500 );
         this.camera.position.set(0, 1.6, 0);
 
-
         
         this.dolly = new THREE.Object3D(  );
         this.dolly.position.set(0, 0, 10);
